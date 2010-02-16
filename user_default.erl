06286ac4867cb4b2168aaedb22673a857ad18dfb
Adam Lindberg <alind@kaze.(none)>
2010-02-16 13:47:36 +0100

    Added dbg wrapper

--- user_default.erl
+++ user_default.erl
@@ -1,5 +1,16 @@
--module (user_default).
--export ([sync/0,make/0,git/1]).
+-module(user_default).
+
+-export([sync/0]).
+-export([make/0]).
+-export([git/1]).
+
+-export([dbg/0]).
+-export([dbg/1]).
+-export([dbg/2]).
+-export([dbg/3]).
+-export([dbg/4]).
+
+-compile(inline).
 
 %% Compiles all files in Emakefile and load into current shell.
 sync() ->
@@ -14,9 +25,38 @@ git(Command) ->
 	CommandList = ["git", Command],
 	run_command(CommandList).
 
+dbg()                           -> dbg:tracer().
+
+dbg(c)                          -> dbg:stop_clear();
+dbg(M)                          -> dbge({M, '_', '_'}, []).
+
+dbg(M, c)                       -> dbgc({M, '_', '_'});
+dbg(M, r)                       -> dbge({M, '_', '_'}, dbg_rt());
+dbg(M, l)                       -> dbgl({M, '_', '_'}, []);
+dbg(M, lr)                      -> dbgl({M, '_', '_'}, dbg_rt());
+dbg(M, O) when is_list(O)       -> dbge({M, '_', '_'}, O);
+dbg(M, F) when is_atom(F)       -> dbge({M,   F, '_'}, []).
+
+dbg(M, F, c)                    -> dbgc({M,   F, '_'});
+dbg(M, F, l)                    -> dbgl({M,   F, '_'}, dbg_rt());
+dbg(M, F, r)                    -> dbge({M,   F, '_'}, dbg_rt());
+dbg(M, F, lr)                   -> dbgl({M,   F, '_'}, dbg_rt());
+dbg(M, F, O) when is_list(O)    -> dbge({M,   F, '_'}, O);
+dbg(M, F, A) when is_integer(A) -> dbge({M,   F,   A}, []).
+
+dbg(M, F, A, c)                 -> dbgc({M,   F,   A});
+dbg(M, F, A, r)                 -> dbge({M,   F,   A}, dbg_rt());
+dbg(M, F, A, l)                 -> dbgl({M,   F,   A}, dbg_rt());
+dbg(M, F, A, lr)                -> dbgl({M,   F,   A}, dbg_rt());
+dbg(M, F, A, O)                 -> dbge({M,   F,   A}, O).
 
 %%%% Private Functions
 
 run_command(CommandList) ->
 	Result = os:cmd(string:join(CommandList, " ")),
 	io:format("~s~n", [Result]).
+
+dbgc(MFA)    -> dbg:ctp(MFA).
+dbge(MFA, O) -> dbg:tracer(), dbg:p(all, call), dbg:tp(MFA, O).
+dbgl(MFA, O) -> dbg:tracer(), dbg:p(all, call), dbg:tpl(MFA, O).
+dbg_rt() -> [{'_',[],[{return_trace}]}].
